746cf1d5d59260fdf896ed2e2054307aba6167d3
heer_ <heer_exc@163.com>
2019-03-10 14:24:03 +0800

    去掉自动配置

--- src/main/java/com/heer/zdclass/HeerZdclassHelperApplication.java
+++ src/main/java/com/heer/zdclass/HeerZdclassHelperApplication.java
@@ -2,10 +2,11 @@ package com.heer.zdclass;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+/**
+ * @author heer_
+ */
 @SpringBootApplication
-@EnableConfigurationProperties
 public class HeerZdclassHelperApplication {
 
 	public static void main(String[] args) {
